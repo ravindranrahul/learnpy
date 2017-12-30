314495bb3858eb55fef5d3430f6eff49bc11a1dc
rahul@awkword.in <rahul@awkword.in>
2017-12-30 22:32:58 +0530

    Added reading speed and updated data

--- learn.py
+++ learn.py
@@ -8,15 +8,38 @@ total_pages = 519
 #ideal spead to finish in 6 days (pages / hr)
 ideal_increment = 519/60
 #pages read per hour
-pages_read_per_hour = [
+reading_progress = [
     10,
-    20
+    19,
+    28,
+    38,
+    50,
+    61,
+    72,
+    72,
+    72,
+    72,
+    77,
+    86,
+    91,
+    91,
+    91,
+    94,
+    100,
+    105,
+    111,
+    122
 ]
+
 #helper function to calculate remaining pages
 def getRemainingPages(n):
   return total_pages-n
-         
-burn_down_chart = pygal.Line()
+  
+burn_down_chart = pygal.Line(
+    legend_at_bottom=True,
+    style=TurquoiseStyle,
+    x_title='No. Of Hours Targeted',
+    y_title='Total No of pages in the book')
 burn_down_chart.title = 'Burndown Chart - python crash course by eric matthes'.title()
 burn_down_chart.x_labels = map(str,range(1,61,1))
 #ideal reading array
@@ -28,5 +51,29 @@ for i in range(1,time_span+1):
 #reverse to predict ideal burndown
 ideal_reading_array.reverse()
 burn_down_chart.add("Ideal Reading Speed",ideal_reading_array)
-burn_down_chart.add("Actual Reading Speed",map(getRemainingPages,pages_read_per_hour))
+burn_down_chart.add("Actual Reading Speed",map(getRemainingPages,reading_progress))
 burn_down_chart.render_to_png('burndown.png')
+
+
+
+
+#calculate pages speed
+pages_per_hour = []
+for i in range(0,len(reading_progress)-2):
+    pages_per_hour.append(reading_progress[i+1]-reading_progress[i]) 
+
+#average speed
+average_speed = str(np.mean(pages_per_hour))
+#trim
+average_speed = average_speed[:3]
+
+#average reading time graph
+reading_speed = pygal.Line(
+    legend_at_bottom=True,
+    style=TurquoiseStyle,
+    x_title='No. Of Hours Targeted',
+    y_title='Reading Speed (Pages)')
+reading_speed.title = 'Reading Speed | Average '+average_speed+'pages/hr'
+reading_speed.x_labels = map(str,range(1,61,1))
+reading_speed.add("Pages Per Hour",pages_per_hour)
+reading_speed.render_to_png('reading_speed.png')
--- readme.md
+++ readme.md
@@ -1,2 +1,2 @@
-# Burndown chart for  Python crash course by eric matthes
 ![Burndown Chart](https://raw.githubusercontent.com/ravindranrahul/learnpy/master/burndown.png)
+![Reading Speed](https://raw.githubusercontent.com/ravindranrahul/learnpy/master/reading_speed.png)
