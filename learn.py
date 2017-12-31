45e979414cda3023d842d4d23c2a982664aba883
rahul@awkword.in <rahul@awkword.in>
2017-12-31 14:04:04 +0530

    Chapter 10 started

--- learn.py
+++ learn.py
@@ -16,9 +16,13 @@ reading_progress = [
     #30 dec
     122,122,122,122,122,122,135,147,153,165,
     #31 dec
-    193
+    193,209,229
     ]
 
+#percentage 
+reading_percentage = reading_progress[-1]/total_pages*100
+reading_percentage = str(reading_percentage)
+reading_percentage =  reading_percentage[:4]
 #helper function to calculate remaining pages
 def getRemainingPages(n):
     return total_pages-n
@@ -28,22 +32,22 @@ burn_down_chart = pygal.Line(
     style=TurquoiseStyle,
     x_title='No. Of Hours Targeted',
     y_title='Total No of pages in the book')
-burn_down_chart.title = 'Burndown Chart - python crash course by eric matthes'.title()
+burn_down_chart.title = 'Burndown Chart: Python Crash Course By Eric Matthes | '+reading_percentage+'% Complete'
 burn_down_chart.x_labels = map(str,range(1,time_span+1))
+
 #ideal reading array
 ideal_reading_array = []
+
 #generate ideal reading array
 for i in range(1,time_span+1):
     current_increment = i*ideal_increment
     ideal_reading_array.append(current_increment) 
+
 #reverse to predict ideal burndown
 ideal_reading_array.reverse()
 burn_down_chart.add("Ideal Reading Speed",ideal_reading_array)
 burn_down_chart.add("Actual Reading Speed",map(getRemainingPages,reading_progress))
 burn_down_chart.render_to_png('burndown.png')
-
-
-
 
 #calculate pages speed
 pages_per_hour = []
@@ -61,7 +65,8 @@ reading_speed = pygal.Line(
     style=TurquoiseStyle,
     x_title='No. Of Hours Targeted',
     y_title='Reading Speed (Pages)')
-reading_speed.title = 'Reading Speed | Average '+average_speed+' pages/hr'
+
+reading_speed.title = 'Reading Speed: Python Crash Course By Eric Matthes | Avg: '+average_speed+' pages/hr'
 reading_speed.x_labels = map(str,range(1,time_span+1))
 reading_speed.add("Pages Per Hour",pages_per_hour)
 reading_speed.render_to_png('reading_speed.png')
